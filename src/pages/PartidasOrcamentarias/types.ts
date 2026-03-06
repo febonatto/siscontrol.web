60faaa394f074acc5b493631e4691ef772cc2ac9
febonatto <fernando.bonatto@dotlib.com>
2026-03-06 08:09:23 -0300

    fix: Ajustado retorno da listagem de Partidas Orçamentárias

--- src/pages/PartidasOrcamentarias/types.ts
+++ src/pages/PartidasOrcamentarias/types.ts
@@ -3,11 +3,16 @@ import {
   InfiniteQuery,
   PaginatedQuery,
   PartidaOrcamentaria,
+  Pessoa,
 } from '@/types';
 import { AxiosResponse } from 'axios';
 
+export type ListPartidasOrcamentarias = PartidaOrcamentaria & {
+  pessoa: Pessoa | null;
+};
+
 export type GetPartidaOrcamentariasResponse = AxiosResponse<
-  PaginatedQuery<PartidaOrcamentaria>
+  PaginatedQuery<ListPartidasOrcamentarias>
 >;
 
 export type GetPartidaOrcamentariaParams = GetPaginatedQueryParams & {
